96f7834d138e439d3f5b780acec08f68c16e9635
juanjo-18 <juanjomedinadiaz@gmail.com>
2022-06-09 13:37:46 +0200

    Ejercicio 6

--- tercer.examen.tercer.trimestre/src/main/java/interfacesgraficas/PantallaEjercicio5.java
+++ tercer.examen.tercer.trimestre/src/main/java/interfacesgraficas/PantallaEjercicio5.java
@@ -4,7 +4,18 @@ import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.TreeSet;
+
 import javax.swing.SwingConstants;
+
+import clases.Preso;
+import excepciones.ValorInvalidoPeligrosidadException;
+import utils.UtilsDB;
 
 import javax.swing.JList;
 import javax.swing.JButton;
@@ -12,6 +23,32 @@ import javax.swing.JButton;
 public class PantallaEjercicio5  extends JPanel{
 	private Ventana ventana;
 	public PantallaEjercicio5(Ventana v) {
+		TreeSet<Preso> presos = new TreeSet<Preso>();
+		
+		Statement smt = UtilsDB.conectarBD();
+		try {
+			ResultSet cursor = smt.executeQuery("select * from preso ");
+			while (cursor.next()) {
+				String dni=cursor.getString("dni");
+
+				String nombre=cursor.getString("nombre");
+				String apellidos=cursor.getString("apellidos");
+				String mote=cursor.getString("mote");
+				byte peligrosidad=cursor.getByte("peligrosidad");
+				Preso preso=new Preso(dni,nombre,apellidos,mote,(byte)peligrosidad);
+				presos.add(preso);
+				
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+		} catch (ValorInvalidoPeligrosidadException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		UtilsDB.desconectarBD();
+		
 		setLayout(new BorderLayout(0, 0));
 		
 		JLabel labelTitulo = new JLabel("Todos los Carceleros");
---
+++ tercer.examen.tercer.trimestre/src/main/java/utils/UtilsDB.java
@@ -0,0 +1,39 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public abstract class UtilsDB {
+	private static final String cadenaConexion = "jdbc:mysql://127.0.0.1:3306/examen090622";
+	private static final String usuarioBD = "root";
+	private static final String contrasenaBD = "contra13";
+	private static Connection conexion;
+
+	public static Statement conectarBD() {
+		try {
+			if (conexion == null) {
+				conexion = DriverManager.getConnection(
+						cadenaConexion, usuarioBD, contrasenaBD);
+			}
+			return conexion.createStatement();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.err.println("No se ha podido conectar, inténtalo luego");
+			return null;
+		}
+	}
+
+	public static void desconectarBD() {
+		if (conexion != null) {
+			try {
+				conexion.close();
+				conexion=null;
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.err.println("No se pudo desconectar");
+			}
+		}
+	}
+}
